88fd44fe7f5c423cdb4462f3e2eb86cb64816a28
Sundeep Gupta <sundeep@sundeepgupta.ca>
2016-04-17 22:53:02 -0400

    Base case

---
+++ dj.playground/Contents.swift
@@ -0,0 +1,13 @@
+class Playlist {
+    private var songs = [
+        "Work",
+        "Stressed Out",
+        "Love Yourself"
+    ]
+    
+    func spin() -> String {
+        return self.songs.joinWithSeparator("\n")
+    }
+}
+
+Playlist().spin()
