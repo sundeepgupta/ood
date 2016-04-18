c24761b9bf4c1b098c1ce5fea478f50e601544a8
Sundeep Gupta <sundeep@sundeepgupta.ca>
2016-04-17 23:11:53 -0400

    Added repeat all

--- dj.playground/Contents.swift
+++ dj.playground/Contents.swift
@@ -1,3 +1,9 @@
+enum RemixType {
+    case None
+    case RepeatEach
+    case RepeatAll
+}
+
 class Playlist {
     private var songs = [
         "Work",
@@ -5,20 +11,29 @@ class Playlist {
         "Love Yourself"
     ]
     
-    func spin(repeatEach repeatEach: Bool) -> String {
-        if repeatEach {
+    func spin(remixType type: RemixType) -> String {
+        switch type {
+        case .RepeatEach:
             var newSongs = [String]()
             for song in self.songs {
                 newSongs.append(song)
                 newSongs.append(song)
             }
             self.songs = newSongs
+            
+        case .RepeatAll:
+            self.songs = self.songs + self.songs
+            
+        case .None:
+            break
         }
         
         return self.songs.joinWithSeparator("\n")
     }
 }
 
-Playlist().spin(repeatEach: true)
+Playlist().spin(remixType: .RepeatEach)
 
-Playlist().spin(repeatEach: false)
+Playlist().spin(remixType: .RepeatAll)
+
+Playlist().spin(remixType: .None)
